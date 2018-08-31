963367f5347271c69934ebcc9b3ee3c6eb59fd0a
Max Nedr <maxim.nedozrelov@ya.ru>
2018-08-31 15:45:15 +0300

    2 задание

--- script.js
+++ script.js
@@ -33,40 +33,77 @@ function Menu(id, className, items) {
 Menu.prototype = Object.create(Container.prototype);
 
 Menu.prototype.render = function () {
+    var div = document.createElement('div');
+
+    div.className = this.className;
+    div.id = this.id;
+
+    this.items.forEach(function (item) {
+        if (item instanceof Container) {
+            div.appendChild(item.render());
+        }
+    });
+
+    return div;
+};
+
+function MenuItem(href, label, id, subItems) {
+    Container.call(this, '', 'menu-item');
+    this.id = id;
+    this.href = href;
+    this.label = label;
+    this.subItems = subItems;
+}
+
+MenuItem.prototype = Object.create(Container.prototype);
+
+MenuItem.prototype.render = function () {
     var ul = document.createElement('ul');
+    var li = document.createElement('li');
+    var a = document.createElement('a');
 
     ul.className = this.className;
     ul.id = this.id;
 
-    this.items.forEach(function (item) {
-        if (item instanceof Container) {
-            ul.appendChild(item.render());
+    a.href = this.href;
+    a.textContent = this.label;
+    ul.appendChild(li);
+    li.appendChild(a);
+    li.className = this.className;
+    console.log(this.subItems);
+    this.subItems.forEach(function (subItem) {
+        if (subItem instanceof Container) {
+            ul.appendChild(subItem.render());
         }
     });
 
     return ul;
 };
 
-function MenuItem(href, label, id) {
+function MenuSubItem(href, label, id) {
     Container.call(this, '', 'menu-item');
     this.id = id;
     this.href = href;
     this.label = label;
-}
 
-MenuItem.prototype = Object.create(Container.prototype);
+};
+MenuSubItem.prototype = Object.create(Container.prototype);
 
-MenuItem.prototype.render = function () {
+MenuSubItem.prototype.render = function () {
+    var ul = document.createElement('ul');
     var li = document.createElement('li');
     var a = document.createElement('a');
 
     a.href = this.href;
     a.textContent = this.label;
 
+    ul.className = this.className;
+    ul.id = this.id;
+
+    ul.appendChild(li);
     li.appendChild(a);
     li.className = this.className;
-    li.id = this.id;
-    return li;
+    return ul;
 };
 
 function Button(id, className, type, value) {
